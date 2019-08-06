024720076e256b10ef5927b118d9596edc5e7b06
Guro <skin.diamond.lol@gmail.com>
2019-08-06 20:42:41 +0300

    javser

--- js/getrelease.js
+++ js/getrelease.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  $.getJSON("https://api.github.com/repos/gurobase/javser/releases/latest").done(function(release) {
+            var asset = release.assets[0];
+            console.log(formatBytes(asset.size));
+            // document.getElementById("downloadButton").innerHTML = formatBytes(asset.size);
+          });
+  
+      });
+  
+
+
 var clicked = false;
 function downloadClick() {
     function GetLatestReleaseInfo() {
@@ -22,6 +34,24 @@ function downloadClick() {
     clicked = true;
 }
 
+var clicked2 = false;
+if (clicked2 === false) {
+  function downloadB() {
+    $.getJSON("https://api.github.com/repos/gurobase/javser/releases/latest").done(function(release) {
+          var asset = release.assets[0];
+          window.open(asset.browser_download_url,"_self");
+        });
+    document.getElementById("btn-download").classList.add("downloaded");
+    clicked2 = true;
+    setTimeout(function() {
+      document.getElementById("btn-download").classList.remove("downloaded");
+      clicked2 = false;
+    }, 3000);
+  }
+}
+
+
+function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
 
 
 
